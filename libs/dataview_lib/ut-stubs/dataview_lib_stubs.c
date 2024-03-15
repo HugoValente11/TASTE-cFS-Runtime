6d35f2af60f2bdbf57c23e78583e6b1b1464abb2
Hugo Valente <hugo.svalente@alumnos.upm.es>
2024-03-15 11:18:06 +0100

    Add unit test stubs for dataview

---
+++ libs/dataview_lib/ut-stubs/dataview_lib_stubs.c
@@ -0,0 +1,30 @@
+/*
+ * The stub functions always share the same API header file as the
+ * main FSW code does.  This should define the same functions
+ * with the same parameters.
+ */
+#include "dataview_init.h"
+
+/*
+ * The "utstubs.h" provides the generic stub framework from UT Assert
+ */
+#include "utstubs.h"
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                 */
+/* Dataview Init function stub                                       */
+/*                                                                 */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+int32 DATAVIEW_Init(void)
+{
+    /*
+     * The UT_DEFAULT_IMPL macro is generally sufficient
+     * for any call that does not have output parameters
+     * and returns an integer status code.
+     *
+     * The default return value is 0, unless the test
+     * case configures something different.
+     */
+    return UT_DEFAULT_IMPL(DATAVIEW_Init);
+
+} /* End DATAVIEW_LIB_Init */
